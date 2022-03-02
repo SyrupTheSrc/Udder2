02fcc17a8a628c9cc78638d62609694cf2247d8c
Nay <SyrupTheSource@gmail.com>
2022-03-02 05:25:55 -0800

    Initial Commit

---
+++ Udder2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder2
+//
+//  Created by Nadir Jackson on 3/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
